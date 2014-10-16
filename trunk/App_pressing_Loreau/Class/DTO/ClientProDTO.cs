46bc8135357a5e451bf0d01a4e21ed5fa5865719
esigelec.ping.1@gmail.com <esigelec.ping.1@gmail.com@f6d911b7-a069-8245-1612-9514350c5421>
2014-10-16 22:29:01 +0000

    Client Pro : Ajout de la méthode allClientPro qui permet d'obtenir la liste de tout les clients Pro existant dans la base de données.

--- trunk/App_pressing_Loreau/Class/DTO/ClientProDTO.cs
+++ trunk/App_pressing_Loreau/Class/DTO/ClientProDTO.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using LoreauApplication.Class.DAO;
+using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,40 @@ namespace LoreauApplication.Class.DTO
 {
     class ClientProDTO
     {
+        public static List<ClientPro> allClientPro(String nom, String prenom, String tel)
+        {
+
+            List<ClientPro> retour = new List<ClientPro>();
+
+            String sql = "SELECT cltp_id, cltp_noment, cltp_fix, cltp_mob, cltp_adresse, cltp_email, cltp_dateinscription FROM clientpro ORDER BY cltp_noment ASC";
+            
+            //connection à la base de données   
+            MySqlConnection connection = Bdd.connexion();
+            MySqlCommand cmd = new MySqlCommand(sql, connection);
+
+            //Execute la commande
+            try
+            {
+                MySqlDataReader msdr = cmd.ExecuteReader();
+                while (msdr.Read())
+                {
+                    retour.Add(new ClientPro(msdr["clt_noment"].ToString(),
+                        msdr["cltp_fix"].ToString(),
+                        msdr["cltp_mob"].ToString(),
+                        msdr["cltp_adresse"].ToString(),
+                        msdr["cltp_email"].ToString(),
+                        DateTime.Parse(msdr["cltp_dateinscription"].ToString())));
+                }
+                msdr.Dispose();
+                return retour;
+            }
+            catch (Exception Ex)
+            {
+                //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+                return null;
+            }
+
+
+        }
     }
 }
