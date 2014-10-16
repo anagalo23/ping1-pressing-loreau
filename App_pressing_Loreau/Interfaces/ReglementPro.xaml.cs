e5b30acd69732b37c3a95db4e9a341e22b521192
alexis.nagalo23@gmail.com <alexis.nagalo23@gmail.com@f6d911b7-a069-8245-1612-9514350c5421>
2014-10-16 15:10:38 +0000

    Ajout de UserControl Modification des pages pour les rendre plus flexible. Ajout d autres pages comme ReglementPro, Administration

---
+++ App_pressing_Loreau/Interfaces/ReglementPro.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace App_pressing_Loreau.Interfaces
+{
+    /// <summary>
+    /// Logique d'interaction pour ReglementPro.xaml
+    /// </summary>
+    public partial class ReglementPro : Window
+    {
+        public ReglementPro()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void huguette( Object sender, EventArgs e)
+        {
+        DragMove();
+        }
+    }
+}
