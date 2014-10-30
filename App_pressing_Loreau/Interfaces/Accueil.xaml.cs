01b4bca1fd2b3e9a977e3018d840199f25e3723d
alexis.nagalo23@gmail.com <alexis.nagalo23@gmail.com@f6d911b7-a069-8245-1612-9514350c5421>
2014-10-30 10:46:18 +0000

    Bouton des utilisateurs avec les couleurs

--- App_pressing_Loreau/Interfaces/Accueil.xaml.cs
+++ App_pressing_Loreau/Interfaces/Accueil.xaml.cs
@@ -21,48 +21,80 @@ namespace App_pressing_Loreau.Interfaces
     /// </summary>
     public partial class Accueil : Window
     {
-        Button user1, user2, user3, user4, user5, user6 ;
+        Button user1, user2, user3, user4, user5, user6, btn;
         public Accueil()
         {
             InitializeComponent();
 
         }
 
-         void CommandBtn_Click(Object sender, RoutedEventArgs e) {   
-            switch (1)
+         void btn_user1_Click(Object sender, EventArgs e)
+        {
+            btn = ((Button)(sender));
+            switch (btn.Name)
             {
 
-                //case "btn_user1":
-                //    btn_user1.Background = Brushes.Red;
-                //    break;
-                //case "btn_user2":
-                //    btn_user1.Background = Brushes.Red;
-                //    break;
-                //case "btn_user3":
-                //    btn_user1.Background = Brushes.Red;
-                //    break;
-                //case "btn_user4":
-                //    btn_user1.Background = Brushes.Red;
-                //    break;
-                //case "btn_user5":
-                //    btn_user1.Background = Brushes.Red;
-                //    break;
-                //case "btn_user6":
-                //    btn_user1.Background = Brushes.Red;
-                //    break;
+                case "btn_user1":
+                    btn_user2.Background = Brushes.Gray;
+                    btn_user3.Background = Brushes.Gray;
+                    btn_user4.Background = Brushes.Gray;
+                    btn_user5.Background = Brushes.Gray;
+                    btn_user6.Background = Brushes.Gray;
+                    btn_user1.Background = Brushes.Red;
+                    break;
+                case "btn_user2":
+                    btn_user2.Background = Brushes.Red;
+                    btn_user1.Background = Brushes.Gray;
+                    btn_user3.Background = Brushes.Gray;
+                    btn_user4.Background = Brushes.Gray;
+                    btn_user5.Background = Brushes.Gray;
+                    btn_user6.Background = Brushes.Gray;
+                    break;
+                case "btn_user3":
+                    btn_user3.Background = Brushes.Red;
+                    btn_user2.Background = Brushes.Gray;
+                    btn_user1.Background = Brushes.Gray;
+                    btn_user4.Background = Brushes.Gray;
+                    btn_user5.Background = Brushes.Gray;
+                    btn_user6.Background = Brushes.Gray;
+                    break;
+                case "btn_user4":
+                    btn_user4.Background = Brushes.Red;
+                    btn_user2.Background = Brushes.Gray;
+                    btn_user3.Background = Brushes.Gray;
+                    btn_user1.Background = Brushes.Gray;
+                    btn_user5.Background = Brushes.Gray;
+                    btn_user6.Background = Brushes.Gray;
+                    break;
+                case "btn_user5":
+                    btn_user5.Background = Brushes.Red;
+                    btn_user2.Background = Brushes.Gray;
+                    btn_user3.Background = Brushes.Gray;
+                    btn_user4.Background = Brushes.Gray;
+                    btn_user1.Background = Brushes.Gray;
+                    btn_user6.Background = Brushes.Gray;
+                    break;
+                case "btn_user6":
+                    btn_user6.Background = Brushes.Red;
+                    btn_user2.Background = Brushes.Gray;
+                    btn_user3.Background = Brushes.Gray;
+                    btn_user4.Background = Brushes.Gray;
+                    btn_user5.Background = Brushes.Gray;
+                    btn_user1.Background = Brushes.Gray;
+                    break;
             }
 
         }
-     
 
-        private void btn_accueil_reception_Click(object sender, RoutedEventArgs e)
+
+        private void btn_accueil_reception_Click(Object sender, RoutedEventArgs e)
         {
             IdentificationClient identificationClient = new IdentificationClient();
             dp.Children.Clear();
             dp.Children.Add(identificationClient);
         }
 
-        private void btn_accueil_rendu_Click(object sender, RoutedEventArgs e)
+        private void btn_accueil_rendu_Click(Object sender, RoutedEventArgs e)
         {
             dp.Children.Clear();
             dp.Children.Add(new RestitutionArticles());
@@ -70,49 +102,56 @@ namespace App_pressing_Loreau.Interfaces
 
         }
 
-        private void btn_accueil_facture_Click(object sender, RoutedEventArgs e)
+        private void btn_accueil_facture_Click(Object sender, RoutedEventArgs e)
         {
             dp.Children.Clear();
 
         }
 
-        private void btn_accueil_client_pro_Click(object sender, RoutedEventArgs e)
+        private void btn_accueil_client_pro_Click(Object sender, RoutedEventArgs e)
         {
 
         }
 
-        private void btn_accueil_administrateur_Click(object sender, RoutedEventArgs e)
+        private void btn_accueil_administrateur_Click(Object sender, RoutedEventArgs e)
         {
             dp.Children.Clear();
             dp.Children.Add(new IdentificationAdmin());
         }
 
-        private void btn_accueil_convoyeur_Click(object sender, RoutedEventArgs e)
+        private void btn_accueil_convoyeur_Click(Object sender, RoutedEventArgs e)
         {
 
         }
 
-        private void btn_accueil_image_Click(object sender, RoutedEventArgs e)
+        private void btn_accueil_image_Click(Object sender, RoutedEventArgs e)
         {
             dp.Children.Clear();
         }
 
-        private void btn_user6_Click(object sender, RoutedEventArgs e)
+        private void btn_accueil_impression_Click(Object sender, RoutedEventArgs e)
         {
 
         }
 
-        private void btn_accueil_impression_Click(object sender, RoutedEventArgs e)
-        {
+        //private void btn_user1_Click(Object sender, EventArgs e)
+        //{
 
-        }
+        //    //if (btn_user1.IsPressed == true)
+        //    //{
 
-        private void btn_user1_Click(object sender, RoutedEventArgs e)
-        {
-            btn_user1.Background = Brushes.Red; 
-        }
-
-
+        //        btn_user1.Background = Brushes.Red;
+        //    //}
+        //    //else 
+        //    //{
+        //    //    btn_user1.Background = Brushes.Yellow;
+        //    //}
 
     }
+
+
 }
+
+
+
+
