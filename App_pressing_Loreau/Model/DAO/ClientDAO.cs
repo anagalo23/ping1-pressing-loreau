64ad6d1b26906715fb02b343617fe0ad6b8b5ef5
esigelec.ping.1@gmail.com <esigelec.ping.1@gmail.com@f6d911b7-a069-8245-1612-9514350c5421>
2014-11-11 15:15:17 +0000

    Client DAO : Fonctionnel. Prepare statement OFF. Suivre le post : http://openclassrooms.com/forum/sujet/c-prepare-statement

--- App_pressing_Loreau/Model/DAO/ClientDAO.cs
+++ App_pressing_Loreau/Model/DAO/ClientDAO.cs
@@ -14,25 +14,25 @@ namespace App_pressing_Loreau.Model.DAO
         {
             MySqlConnection connection = Bdd.connexion();
 
-            String sql = "INSERT INTO client(clt_nom, clt_prenom, clt_fix, clt_mob, clt_adresse, clt_dateNaissance, clt_email, clt_dateInscription, clt_idCleanway, clt_contactmail, clt_contactsms) VALUES (\"@nom\",\"@prenom\",\"@telfix\",\"@telport\",\"@adresse\",\"@dateNaissance\",\"@email\",\"@dateInsc\",\"@idCleanWay\",\"@contactMail\",\"@contactSms\")";
-
+            //String sql = "INSERT INTO client(clt_nom, clt_prenom, clt_fix, clt_mob, clt_adresse, clt_dateNaissance, clt_email, clt_dateInscription, clt_idCleanway, clt_contactmail, clt_contactsms) VALUES (\"@nom\",\"@prenom\",\"@telfix\",\"@telport\",\"@adresse\",\"@dateNaissance\",\"@email\",\"@dateInsc\",\"@idCleanWay\",\"@contactMail\",\"@contactSms\")";
+            String sql = "INSERT INTO client(clt_nom, clt_prenom, clt_fix, clt_mob, clt_adresse, clt_dateNaissance, clt_email, clt_dateInscription, clt_idCleanway, clt_contactmail, clt_contactsms) VALUES (\"" + client.nom + "\",\"" + client.prenom + "\",\"" + client.telfix + "\",\"" + client.telmob + "\",\"" + client.adresse + "\",\"" + String.Format("{0:YYYYMMddHHmmss}", client.dateNaissance) + "\",\"" + client.email + "\",\"" + String.Format("{0:YYYYMMddHHmmss}", client.dateInscription) + "\",\"" + client.idCleanWay + "\",\"" + client.contactMail + "\",\"" + client.contactSms + "\")";
             //connection à la base de données   
-            
+
             MySqlCommand cmd = new MySqlCommand(sql, connection);
             //cmd.Prepare();
             cmd.CommandText = sql;
             //ajout des parametres
-            cmd.Parameters.AddWithValue("@nom", client.nom);
-            cmd.Parameters.AddWithValue("@prenom", client.prenom);
-            cmd.Parameters.AddWithValue("@telfixe", client.telfix);
-            cmd.Parameters.AddWithValue("@telport", client.telmob);
-            cmd.Parameters.AddWithValue("@adresse", client.adresse);
-            cmd.Parameters.AddWithValue("@dateNaissance", String.Format("{0:YYYYMMddHHmmss}", client.dateNaissance)); //parametre date sous format annee + mois + jour + heure + minute + seconde
-            cmd.Parameters.AddWithValue("@email", client.email);
-            cmd.Parameters.AddWithValue("@dateInsc", String.Format("{0:YYYYMMddHHmmss}", client.dateInscription)); //parametre date sous format annee + mois + jour + heure + minute + seconde
-            cmd.Parameters.AddWithValue("@idCleanWay", client.idCleanWay);
-            cmd.Parameters.AddWithValue("@contactMail", client.contactMail);
-            cmd.Parameters.AddWithValue("@contactSms", client.contactSms);
+            //cmd.Parameters.AddWithValue("@nom", client.nom);
+            //cmd.Parameters.AddWithValue("@prenom", client.prenom);
+            //cmd.Parameters.AddWithValue("@telfixe", client.telfix);
+            //cmd.Parameters.AddWithValue("@telport", client.telmob);
+            //cmd.Parameters.AddWithValue("@adresse", client.adresse);
+            //cmd.Parameters.AddWithValue("@dateNaissance", String.Format("{0:YYYYMMddHHmmss}", client.dateNaissance)); //parametre date sous format annee + mois + jour + heure + minute + seconde
+            //cmd.Parameters.AddWithValue("@email", client.email);
+            //cmd.Parameters.AddWithValue("@dateInsc", String.Format("{0:YYYYMMddHHmmss}", client.dateInscription)); //parametre date sous format annee + mois + jour + heure + minute + seconde
+            //cmd.Parameters.AddWithValue("@idCleanWay", client.idCleanWay);
+            //cmd.Parameters.AddWithValue("@contactMail", client.contactMail);
+            //cmd.Parameters.AddWithValue("@contactSms", client.contactSms);
             int retour = cmd.ExecuteNonQuery();
             connection.Close();
 
@@ -47,7 +47,7 @@ namespace App_pressing_Loreau.Model.DAO
                 return 0;
             }
         }
-     
+
 
         public static List<Client> seekClients(String nom, String prenom, String tel)
         {
@@ -62,19 +62,20 @@ namespace App_pressing_Loreau.Model.DAO
             #region complete la requete en fonction de la recherche voulue
             if (nom != null)
             {
-                sql = String.Format("{0}{1}", sql, "clt_nom=\"@nom\"");
+                //sql = String.Format("{0}{1}", sql, "clt_nom=\"@nom\"");
+                sql = String.Format("{0}{1}", sql, "clt_nom=\"" + nom + "\"");
                 ifPreviousElementSearch = true;
             }
             if (prenom != null)
             {
                 if (ifPreviousElementSearch) sql = String.Format("{0}{1}", sql, " AND ");
-                sql = String.Format("{0}{1}", sql, "clt_nom=\"@prenom\"");
+                sql = String.Format("{0}{1}", sql, "clt_nom=\"" + prenom + "\"");
                 ifPreviousElementSearch = true;
             }
             if (tel != null)
             {
                 if (ifPreviousElementSearch) sql = String.Format("{0}{1}", sql, " AND ");
-                sql = String.Format("{0}{1}", sql, "(clt_fix=\"@tel\" OR clt_mob = \"@tel\")");
+                sql = String.Format("{0}{1}", sql, "(clt_fix=\"" + tel + "\" OR clt_mob = \"" + tel + "\")");
                 ifPreviousElementSearch = true;
             }
             if (!ifPreviousElementSearch) sql = String.Format("{0}{1}", sql, "1=1");
@@ -86,32 +87,32 @@ namespace App_pressing_Loreau.Model.DAO
             MySqlCommand cmd = new MySqlCommand(sql, connection);
 
             //ajout des parametres
-            cmd.Parameters.AddWithValue("nom", nom);
-            cmd.Parameters.AddWithValue("prenom", prenom);
-            cmd.Parameters.AddWithValue("tel", tel);
+            //cmd.Parameters.AddWithValue("nom", nom);
+            //cmd.Parameters.AddWithValue("prenom", prenom);
+            //cmd.Parameters.AddWithValue("tel", tel);
 
             //Execute la commande
             //try
             //{
-                MySqlDataReader msdr = cmd.ExecuteReader();
-                Client client;
-                while (msdr.Read())
-                {
-                    client = new Client(msdr["clt_nom"].ToString(),
-                        msdr["clt_prenom"].ToString(),
-                        msdr["clt_fix"].ToString(),
-                        msdr["clt_mob"].ToString(),
-                        msdr["clt_adresse"].ToString(),
-                        DateTime.Parse(msdr["clt_dateNaissance"].ToString()),
-                        msdr["clt_email"].ToString(),
-                        DateTime.Parse(msdr["clt_dateInscription"].ToString()),
-                        Int32.Parse(msdr["clt_idCleanway"].ToString()));
-                    client.setContactMail(Int32.Parse(msdr["clt_contactmail"].ToString()));
-                    client.setContactSms(Int32.Parse(msdr["clt_contactsms"].ToString()));
-                    retour.Add(client);
-                }
-                msdr.Dispose();
-                return retour;
+            MySqlDataReader msdr = cmd.ExecuteReader();
+            Client client;
+            while (msdr.Read())
+            {
+                client = new Client(msdr["clt_nom"].ToString(),
+                    msdr["clt_prenom"].ToString(),
+                    msdr["clt_fix"].ToString(),
+                    msdr["clt_mob"].ToString(),
+                    msdr["clt_adresse"].ToString(),
+                    DateTime.Parse(msdr["clt_dateNaissance"].ToString()),
+                    msdr["clt_email"].ToString(),
+                    DateTime.Parse(msdr["clt_dateInscription"].ToString()),
+                    Int32.Parse(msdr["clt_idCleanway"].ToString()));
+                client.setContactMail(Int32.Parse(msdr["clt_contactmail"].ToString()));
+                client.setContactSms(Int32.Parse(msdr["clt_contactsms"].ToString()));
+                retour.Add(client);
+            }
+            msdr.Dispose();
+            return retour;
             //}
             //catch (Exception Ex)
             //{
