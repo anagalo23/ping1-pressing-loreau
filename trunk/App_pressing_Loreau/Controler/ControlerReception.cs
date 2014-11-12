b86d2d5a70304f9e63604c409625184c9552af2f
p.taquet@groupe-esigelec.fr <p.taquet@groupe-esigelec.fr@f6d911b7-a069-8245-1612-9514350c5421>
2014-11-12 20:17:02 +0000

    insertCommande dans la DAO et implémentation du controleur (début)

--- trunk/App_pressing_Loreau/Controler/ControlerReception.cs
+++ trunk/App_pressing_Loreau/Controler/ControlerReception.cs
@@ -1,9 +1,11 @@
-﻿using App_pressing_Loreau.Model.DTO;
+﻿using App_pressing_Loreau.Model.DAO;
+using App_pressing_Loreau.Model.DTO;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 
 namespace App_pressing_Loreau.Controler
 {
@@ -11,9 +13,61 @@ namespace App_pressing_Loreau.Controler
     {
         public void ajoutCommande(Commande commande)
         {
-            int idClt = commande.client.id;
+            //Command contain : 
+            //  -   each articles (List of articles)
+            //  -   the client
+
+            // Steps for add the command in the Database
+            //  -   Assert each field of the object command is fill in, except client ids
+            //  -   Initialize the field 'payee'
+            //  -   Save the command in Database
+            //  -   Get his id
+            //  -   Complete 'articles' objects
+            //  -   We don't care the 'paiement', by default it's not paid
+
+            //Initialize the command field 'payee' to false
+            commande.payee = false;
+
+            //Get the client id
+            int idClt;
+            if ((commande.client.id == null) && (commande.clientpro.id != null))
+            {
+                idClt = commande.clientpro.id;
+            }
+            else if ((commande.client.id != null) && (commande.clientpro.id == null))
+            {
+                idClt = commande.client.id;
+            }
+            else
+            {
+                //Both Ids are null or both are initialized. Theses impossibles cases are to be avoided
+                throw new Exception("Problème dans la construction de la commande : soit pas de client, soit deux. Faudrait savoir...");
+            }
+            //Assert the integrity ------------------> TODO
+
+            //Add the command in the dataBase
+            CommandeDAO.insertCommande(commande);
+
+            //Get the command id ------------------> TODO
+            // Good question 
+            // Check commende.id = ?
+
+            //Set paiement  ------------------------> TODO
 
 
+            //Set articles --> Review architecture, theses lines have not to be here
+            // perhaps : public static int insertArticles(List<Article> articles);
+            //ArticleDAO.insertArticles(listArticles);
+            List<Article> listArticles = commande.listArticles;// <-- penser à add les fk pour bound la commande
+            //for (int i = 0; i < listArticles.Count; i++)
+            //{
+            //    ArticleDAO.insertArticle(listArticles[i]);
+            //}
+            // Better ;-)
+            foreach (Article article in listArticles)
+            {
+                ArticleDAO.insertArticle(article);
+            }
         }
     }
 }
--- trunk/App_pressing_Loreau/Model/DAO/CommandeDAO.cs
+++ trunk/App_pressing_Loreau/Model/DAO/CommandeDAO.cs
@@ -4,12 +4,43 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using App_pressing_Loreau.Model.DTO;
+using MySql.Data.MySqlClient;
 
 namespace App_pressing_Loreau.Model.DAO
 {
     class CommandeDAO
     {
         //public static List<Commande> get
+
+        public static int insertCommande(Commande commande)
+        {
+            MySqlConnection connection = Bdd.connexion();
+            String sql = "INSERT INTO commande (cmd_date, cmd_payee) Values (\"@cmd_date\", \"@cmd_payee\")";
+
+            //connection à la base de données   
+
+            MySqlCommand cmd = new MySqlCommand(sql, connection);
+            // Le langage d'insertion en bdd est le sql
+            cmd.CommandText = sql;
+            //ajout des parametres
+            cmd.Parameters.AddWithValue("@cmd_date", commande.date);
+            cmd.Parameters.AddWithValue("@cmd_payee", commande.payee);
+
+            int retour;
+
+
+            try
+            {
+                retour = cmd.ExecuteNonQuery();
+                connection.Close();
+                return retour;
+            }
+            catch
+            {
+                return 0;
+            }
+
+        }
 
     }
 }
