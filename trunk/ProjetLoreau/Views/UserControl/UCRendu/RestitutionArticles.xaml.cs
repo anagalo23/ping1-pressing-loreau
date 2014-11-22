c427946d0b6c69df3d906667a1dd288264a7bc09
esigelec.ping.1@gmail.com <esigelec.ping.1@gmail.com@f6d911b7-a069-8245-1612-9514350c5421>
2014-11-22 09:16:52 +0000

    Changement références images

--- trunk/ProjetLoreau/Views/UserControl/UCRendu/RestitutionArticles.xaml.cs
+++ trunk/ProjetLoreau/Views/UserControl/UCRendu/RestitutionArticles.xaml.cs
@@ -13,7 +13,6 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Data;
-using ProjetLoreau.Controler;
 using ProjetLoreau.Model.DTO;
 using ProjetLoreau.Model.DAO;
 
