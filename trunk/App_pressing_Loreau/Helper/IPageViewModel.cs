77e2700be3ec9582de5d3d6add80486b2162d1ed
alexis.nagalo23@gmail.com <alexis.nagalo23@gmail.com@f6d911b7-a069-8245-1612-9514350c5421>
2014-11-23 01:28:02 +0000

    Ajout de Helper et les classes IPageViewModel.cs, ObservableObject.cs,RelayCommand.cs et AccueilVM.cs

---
+++ trunk/App_pressing_Loreau/Helper/IPageViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App_pressing_Loreau.Helper
+{
+    class IPageViewModel
+    {
+        string Name { get; }
+    }
+}
