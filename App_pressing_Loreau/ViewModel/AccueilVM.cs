8107c968e5f76543f573fc998c29ef4f1b3ed9f0
alexis.nagalo23@gmail.com <alexis.nagalo23@gmail.com@f6d911b7-a069-8245-1612-9514350c5421>
2014-11-27 02:55:26 +0000

    Modification et utilisation des ViewModels pour en tenir compte de la suite du projet

--- App_pressing_Loreau/ViewModel/AccueilVM.cs
+++ App_pressing_Loreau/ViewModel/AccueilVM.cs
@@ -1,29 +1,37 @@
-﻿using System;
+﻿using App_pressing_Loreau.Helper;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using App_pressing_Loreau.Helper;
+using System.Windows.Controls;
 using System.Windows.Input;
+using App_pressing_Loreau;
+using System.Windows;
+using App_pressing_Loreau.View;
 
-namespace App_pressing_Loreau.ViewModel
+namespace App_pressing_Loreau
 {
-    class AccueilVM : ObservableObject
+   public class AccueilVM : ObservableObject
     {
         #region Fields
-
         private ICommand _changePageCommand;
-
+        Accueil ac;
         private IPageViewModel _currentPageViewModel;
         private List<IPageViewModel> _pageViewModels;
 
         #endregion
 
+        #region Constructeur 
         public AccueilVM () 
         {
            // PageViewModels.Add(this);
+            PageViewModels.Add(new IdentificationClientVM());
+          
 
         }
+
+        #endregion
 
         #region Properties / Commands
 
@@ -42,6 +50,23 @@ namespace App_pressing_Loreau.ViewModel
             }
         }
 
+
+        ICommand onCollectionChangeCommand;
+        public ICommand OnCollectionChangeCommand
+        {
+            get { return onCollectionChangeCommand ?? (onCollectionChangeCommand = new RelayCommand(OnCollectionChange)); }
+        }
+        private void OnCollectionChange(object lang)
+        {
+            ac = new Accueil();
+            if (lang.ToString().Equals("btn_accueil_reception"))
+            {
+                CurrentPageViewModel = PageViewModels[0];
+                
+               //ac.dpanel.Children.Clear();
+               //ac.dpanel.Children.Add(new IdentificationClient());
+            }
+        }
         public List<IPageViewModel> PageViewModels
         {
             get
@@ -85,4 +110,12 @@ namespace App_pressing_Loreau.ViewModel
         #endregion
 
     }
+
+
+   public class CategoryItem
+   {
+       public string ButtonContent { get; set; }
+       public string ButtonTag { get; set; }
+   }
+
 }
