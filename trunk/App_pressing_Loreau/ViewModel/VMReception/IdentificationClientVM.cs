71177acd83cea08434634bd6e1175d00168adce5
alexis.nagalo23@gmail.com <alexis.nagalo23@gmail.com@f6d911b7-a069-8245-1612-9514350c5421>
2014-12-04 14:43:24 +0000

    Commit pour des corrections mineurs

--- trunk/App_pressing_Loreau/ViewModel/VMReception/IdentificationClientVM.cs
+++ trunk/App_pressing_Loreau/ViewModel/VMReception/IdentificationClientVM.cs
@@ -3,15 +3,84 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using App_pressing_Loreau.Helper;
+using System.Windows.Input;
 
 namespace App_pressing_Loreau.View
 {
     class IdentificationClientVM : ObservableObject, IPageViewModel
     {
+
+        #region Variables
+
+        private String _txt_identificationClient_nom;
+        private IdentificationClientData _resultatRecherche;
+
+        private ICommand _btnIdentifiantNouveauClient;
+        #endregion
+
         public string Name
         {
             get { return ""; }
         }
+
+        public IdentificationClientVM(){
+          
+        }
+
+         #region Properties 
+
+        public String txb_identificationClient_nom
+        {
+            get { return _txt_identificationClient_nom; }
+            set
+            {
+                if (value != _txt_identificationClient_nom)
+                {
+                    _txt_identificationClient_nom = value;
+                    OnPropertyChanged("txb_identificationClient_nom");
+                }
+            }
+        }
+
+
+        public IdentificationClientData resultatRecherche
+        {
+            get { return _resultatRecherche; }
+            set
+            {
+                if (value != _resultatRecherche)
+                {
+                    _resultatRecherche = value;
+                    OnPropertyChanged("resultatRecherche");
+                }
+            }
+        }
+
+        public ICommand btnIdentifiantNouveauClient
+        {
+            get
+            {
+                if (_btnIdentifiantNouveauClient != null)
+                {
+                    _btnIdentifiantNouveauClient = new RelayCommand(
+                        p=>resultatName());
+
+                }
+                return _btnIdentifiantNouveauClient;
+            }
+
+        }
+         #endregion
+
+
+        #region methodes
+        private void resultatName()
+        {
+            IdentificationClientData icd = new IdentificationClientData();
+            icd.txb_identificationClient_nom= txb_identificationClient_nom;
+            resultatRecherche = icd;
+        }
+        #endregion
 
 
 
