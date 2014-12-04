034e0446ddf683e8b929ba6f0ffe92f8fb6bc8c2
h.fofou.12@groupe-esigelec.fr <h.fofou.12@groupe-esigelec.fr@f6d911b7-a069-8245-1612-9514350c5421>
2014-12-04 15:34:22 +0000

    variable

--- App_pressing_Loreau/ViewModel/VMReception/IdentificationClientData.cs
+++ App_pressing_Loreau/ViewModel/VMReception/IdentificationClientData.cs
@@ -12,6 +12,11 @@ namespace App_pressing_Loreau.View
         #region Variables
 
         private String _txt_identificationClient_nom;
+        private String _txt_identificationClient_prenom;
+        private String _txt_identificationClient_portable;
+        private String _txt_identificationClient_adresse;
+        private String _txt_identificationClient_date_naissance;
+        
         #endregion
 
 
@@ -30,6 +35,57 @@ namespace App_pressing_Loreau.View
             }
         }
 
+        public String txb_identificationClient_prenom
+        {
+            get { return _txt_identificationClient_prenom; }
+            set
+            {
+                if (value != _txt_identificationClient_prenom)
+                {
+                    _txt_identificationClient_prenom = value;
+                    OnPropertyChanged("txb_identificationClient_prenom");
+                }
+            }
+        }
+
+        public String txb_identificationClient_portable
+        {
+            get { return _txt_identificationClient_portable; }
+            set
+            {
+                if (value != _txt_identificationClient_portable)
+                {
+                    _txt_identificationClient_portable = value;
+                    OnPropertyChanged("txb_identificationClient_portable");
+                }
+            }
+        }
+
+        public String txb_identificationClient_adresse
+        {
+            get { return _txt_identificationClient_adresse; }
+            set
+            {
+                if (value != _txt_identificationClient_adresse)
+                {
+                    _txt_identificationClient_adresse = value;
+                    OnPropertyChanged("txb_identificationClient_adresse");
+                }
+            }
+        }
+
+        public String txb_identificationClient_date_naissance
+        {
+            get { return _txt_identificationClient_date_naissance; }
+            set
+            {
+                if (value != _txt_identificationClient_date_naissance)
+                {
+                    _txt_identificationClient_date_naissance = value;
+                    OnPropertyChanged("txb_identificationClient_date_naissance");
+                }
+            }
+        }
         #endregion
 
      
