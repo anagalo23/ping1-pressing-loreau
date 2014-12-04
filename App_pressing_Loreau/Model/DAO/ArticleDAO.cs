ef8b82358d63ee1a137e28c2c44e30d033194dfd
esigelec.ping.1@gmail.com <esigelec.ping.1@gmail.com@f6d911b7-a069-8245-1612-9514350c5421>
2014-12-04 11:25:52 +0000

    Article DAO : insert & select by id OK

--- App_pressing_Loreau/Model/DAO/ArticleDAO.cs
+++ App_pressing_Loreau/Model/DAO/ArticleDAO.cs
@@ -10,70 +10,85 @@ namespace App_pressing_Loreau.Model.DAO
 {
     class ArticleDAO
     {
-        public static int insertArticle(Article article)
+        public static void insertArticle(Article article)
         {
-            MySqlConnection connection = Bdd.connexion();
-
-
-            String sql = "INSERT INTO article (art_photo, art_commentaire, art_rendu, convoyeur_conv_id,"
-                           + "departement_dep_id, type_typ_id) VALUES (\"@photo\",\"@comm\",\"@rendu\",\"@conv_id\",\"@dep_id\",\"@typ_id\")";
-
-            MySqlCommand cmd = new MySqlCommand(sql, connection);
-            //cmd.Prepare();
-            cmd.CommandText = sql;
-            //ajout des parametres
-            cmd.Parameters.AddWithValue("@photo", article.photo);
-            cmd.Parameters.AddWithValue("@comm", article.commentaire);
-            cmd.Parameters.AddWithValue("@rendu", article.ifRendu);
-
-
-            int retour = cmd.ExecuteNonQuery();
-            connection.Close();
             try
             {
-                return retour;
+                String sql = "INSERT INTO article(art_photo, art_commentaire, art_rendu, art_TVA, art_HT, art_conv_id, art_typ_id) VALUES (?,?,?,?,?,?,?)";
+
+                //connection à la base de données
+                MySqlConnection connection = Bdd.connexion();
+                MySqlCommand cmd = new MySqlCommand(sql, connection);
+
+                //ajout des parametres
+                cmd.Parameters.AddWithValue("photo", article.photo);
+                cmd.Parameters.AddWithValue("commentaire", article.commentaire);
+                cmd.Parameters.AddWithValue("rendu", article.ifRendu);
+                cmd.Parameters.AddWithValue("TVA", article.TVA);
+                cmd.Parameters.AddWithValue("HT", article.HT);
+                cmd.Parameters.AddWithValue("conv_id", article.convoyeur.id);
+                cmd.Parameters.AddWithValue("typ_id", article.type.id);
+
+                //Execute la commande
+                int retour = cmd.ExecuteNonQuery();
+                connection.Close();
             }
-            catch
+            catch (Exception Ex)
             {
-                return 0;
+                LogDAO.insertLog(new Log(DateTime.Now, "ERREUR BDD : Erreur dans l'insertion d'un article dans la base de données."));
             }
         }
 
-        public static List<Article> getArticlesById(int art_id)
+        public static Article getArticleById(int art_id)
         {
-            MySqlConnection connection = Bdd.connexion();
-
-            List<Article> listArticle = new List<Article>();
-
-            String sql = "SELECT art_id, art_photo, art_commentaire, art_rendu, convoyeur_conv_id,"
-                           + "departement_dep_id, type_typ_id FROM article WHERE =" + art_id;
-
-            MySqlCommand cmd = new MySqlCommand(sql, connection);
-            //cmd.Prepare();
-            cmd.CommandText = sql;
-
-            //Execute la commande
             try
             {
+                Article retour = new Article();
+                String sql = "SELECT A.art_id, A.art_photo, A.art_commentaire, A.art_rendu, A.art_TVA, A.art_HT, C.conv_id, C.conv_emplacement, T.typ_id, T.type_nom, T.type_encombrement, T.type_TVA, T.type_HT, T.type_dep_id, D.dep_nom FROM article A, convoyeur C, type T, departement D WHERE A.art_conv_id=C.conv_id AND A.art_typ_id=T.typ_id AND T.type_dep_id=D.dep_id AND A.art_id=?";
+
+                //connection à la base de données
+                MySqlConnection connection = Bdd.connexion();
+                MySqlCommand cmd = new MySqlCommand(sql, connection);
+
+                //ajout des parametres
+                cmd.Parameters.AddWithValue("id", art_id);
+
+                //Execute la commande
                 MySqlDataReader msdr = cmd.ExecuteReader();
-                Article article;
                 while (msdr.Read())
                 {
-                    //article = new Article(Int32.Parse(msdr["art_id"].ToString()),
-                    //    msdr["art_photo"].ToString(), msdr["art_commentaire"].ToString(),
-                    //     bool.Parse(msdr["art_rendu"].ToString()), Int32.Parse(msdr["type_typ_id"].ToString()),
-                    //     Int32.Parse(msdr["departement_dep_id"].ToString()), Int32.Parse(msdr["convoyeur_conv_id"].ToString()));
-                    //listArticle.Add(article);
+                    Departement dep = new Departement(
+                        Int32.Parse(msdr["type_dep_id"].ToString()),
+                        msdr["dep_nom"].ToString());
+                    
+                    TypeArticle type = new TypeArticle(
+                        Int32.Parse(msdr["typ_id"].ToString()), 
+                        msdr["type_nom"].ToString() , 
+                        float.Parse(msdr["type_encombrement"].ToString()), 
+                        float.Parse(msdr["type_TVA"].ToString()), 
+                        float.Parse(msdr["type_HT"].ToString()), 
+                        dep);
+                    PlaceConvoyeur conv = new PlaceConvoyeur(
+                        Int32.Parse(msdr["conv_id"].ToString()), 
+                        Int32.Parse(msdr["conv_emplacement"].ToString()));
+                    retour = new Article(
+                        Int32.Parse(msdr["art_id"].ToString()), 
+                        msdr["art_photo"].ToString(), 
+                        msdr["art_commentaire"].ToString(), 
+                        bool.Parse(msdr["art_rendu"].ToString()), 
+                        float.Parse(msdr["art_TVA"].ToString()), 
+                        float.Parse(msdr["art_HT"].ToString()), 
+                        type, 
+                        conv);
                 }
                 msdr.Dispose();
-
+                return retour;
             }
-            catch
+            catch (Exception Ex)
             {
-               
+                LogDAO.insertLog(new Log(DateTime.Now, "ERREUR BDD : Erreur dans la selection d'un article dans la base de données."));
+                return null;
             }
-            return listArticle;
-
         }
     }
 }
