bd0c9458e83a2314c32d18b9643266a3aa13253b
alexis.nagalo23@gmail.com <alexis.nagalo23@gmail.com@f6d911b7-a069-8245-1612-9514350c5421>
2014-12-18 14:18:36 +0000

    Commit Modif TypeArticle et TypeArticleDAO.cs et quelques modif

--- App_pressing_Loreau/Model/DAO/TypeArticleDAO.cs
+++ App_pressing_Loreau/Model/DAO/TypeArticleDAO.cs
@@ -79,12 +79,15 @@ namespace App_pressing_Loreau.Model.DAO
             }
         }
 
+        // Modifier par Alexis
+        //Dans notre application, on recuprere l'article par departement
+        /*
         public static TypeArticle selectTypeById(int id)
         {
             try
             {
                 TypeArticle retour = new TypeArticle();
-                String sql = "SELECT T.typ_id, T.type_nom, T.type_encombrement, T.type_TVA, T.type_HT, T.type_dep_id, D.dep_nom FROM type T, departement D WHERE T.type_dep_id=D.dep_id AND T.typ_id=?";
+                String sql = "SELECT T.typ_id, T.typ_nom, T.typ_encombrement, T.typ_TVA, T.typ_HT, T.typ_dep_id, D.dep_nom FROM type T, departement D WHERE T.typ_dep_id=D.dep_id AND T.typ_id=?";
 
                 //connection à la base de données
                 MySqlConnection connection = Bdd.connexion();
@@ -99,15 +102,54 @@ namespace App_pressing_Loreau.Model.DAO
                 while (msdr.Read())
                 {
                     departement = new Departement(
-                        Int32.Parse(msdr["type_dep_id"].ToString()),
-                        msdr["dep_nom"].ToString());
+                       Int32.Parse(msdr["typ_dep_id"].ToString()),
+                       msdr["dep_nom"].ToString());
+                    retour = new TypeArticle(
+                        Int32.Parse(msdr["typ_id"].ToString()),
+                        msdr["typ_nom"].ToString(),
+                        float.Parse(msdr["typ_encombrement"].ToString()),
+                        float.Parse(msdr["typ_TVA"].ToString()),
+                        float.Parse(msdr["typ_HT"].ToString()),
+                        departement);
+                }
+                msdr.Dispose();
+                return retour;
+            }
+            catch (Exception Ex)
+            {
+                LogDAO.insertLog(new Log(DateTime.Now, "ERREUR BDD : Erreur dans la selection d'un département dans la base de données."));
+                return null;
+            }
+        }
+
+        */
+        public static TypeArticle selectTypeByIdDep(int idDep)
+        {
+            try
+            {
+                TypeArticle retour = new TypeArticle();
+                String sql = "SELECT T.typ_id, T.typ_nom, T.typ_encombrement, T.typ_TVA, T.typ_HT, T.typ_dep_id FROM type T WHERE  T.typ_dep_id=?";
+
+                //connection à la base de données
+                MySqlConnection connection = Bdd.connexion();
+                MySqlCommand cmd = new MySqlCommand(sql, connection);
+
+                //ajout des parametres
+                cmd.Parameters.AddWithValue("id", idDep);
+
+                //Execute la commande
+                MySqlDataReader msdr = cmd.ExecuteReader();
+               // Departement departement;
+                while (msdr.Read())
+                {
+                   
                     retour = new TypeArticle(
                         Int32.Parse(msdr["typ_id"].ToString()),
                         msdr["type_nom"].ToString(),
-                        float.Parse(msdr["type_encombrement"].ToString()),
-                        Int32.Parse(msdr["type_TVA"].ToString()),
-                        Int32.Parse(msdr["type_HT"].ToString()),
-                        departement);
+                        float.Parse(msdr["typ_encombrement"].ToString()),
+                        float.Parse(msdr["typ_TVA"].ToString()),
+                        float.Parse(msdr["typ_HT"].ToString()),
+                        Int32.Parse(msdr["typ_dep_id"].ToString()));
                 }
                 msdr.Dispose();
                 return retour;
