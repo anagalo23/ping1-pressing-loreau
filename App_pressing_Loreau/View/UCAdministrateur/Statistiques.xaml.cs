425c8b6ca9b20202120ab8399e7527f682d51ec1
p.taquet@groupe-esigelec.fr <p.taquet@groupe-esigelec.fr@f6d911b7-a069-8245-1612-9514350c5421>
2015-01-10 14:13:08 +0000

    commit

--- App_pressing_Loreau/View/UCAdministrateur/Statistiques.xaml.cs
+++ App_pressing_Loreau/View/UCAdministrateur/Statistiques.xaml.cs
@@ -12,7 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
-namespace App_pressing_Loreau
+namespace App_pressing_Loreau.View
 {
     /// <summary>
     /// Logique d'interaction pour Statistiques.xaml
