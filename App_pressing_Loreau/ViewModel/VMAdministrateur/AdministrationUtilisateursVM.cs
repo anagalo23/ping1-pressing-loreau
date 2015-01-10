aef8e2fd3d23003a444da66bf38e8d137646b981
alexis.nagalo23@gmail.com <alexis.nagalo23@gmail.com@f6d911b7-a069-8245-1612-9514350c5421>
2015-01-10 20:49:52 +0000

    Creation de la classe AdministrationClient ,AdministrationUtilisateursVM et modif minim

---
+++ App_pressing_Loreau/ViewModel/VMAdministrateur/AdministrationUtilisateursVM.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using App_pressing_Loreau.Helper;
+using App_pressing_Loreau.Data.DAO;
+using App_pressing_Loreau.Model.DTO;
+
+namespace App_pressing_Loreau.ViewModel
+{
+    class AdministrationUtilisateursVM : ObservableObject, IPageViewModel
+    {
+        private List<Utilisateur> _listeUtilisateurs;
+        private String _nameUtilisateur;
+
+        public String Name
+        {
+            get { return ""; }
+        }
+
+        public AdministrationUtilisateursVM()
+        {
+            initialisationUtilisateurs();   
+        }
+
+        #region properties and commands
+
+        //public String NameUtilisateur
+        //{
+        //    get { return _nameUtilisateur; }
+        //    set
+        //    {
+        //        if (value != _nameUtilisateur)
+        //        {
+        //            _nameUtilisateur = value;
+        //            OnPropertyChanged("NameUtilisateur");
+        //        }
+        //    }
+        //}
+        public List<Utilisateur> ListeUtilisateurs
+        {
+            get { return _listeUtilisateurs; }
+            set
+            {
+                _listeUtilisateurs = value;
+                RaisePropertyChanged("ListeUtilisateurs");
+            }
+        }
+
+        #endregion
+
+        #region Methods
+        public void initialisationUtilisateurs()
+        {
+            ListeUtilisateurs = new List<Utilisateur>();
+            ListeUtilisateurs.Add(new Utilisateur() { NameUtilisateur = "Alexis" });
+            ListeUtilisateurs.Add(new Utilisateur() { NameUtilisateur = "Huguette" });
+            ListeUtilisateurs.Add(new Utilisateur() { NameUtilisateur = "Pierre" });
+            ListeUtilisateurs.Add(new Utilisateur() { NameUtilisateur = "Pierre" });
+            ListeUtilisateurs.Add(new Utilisateur() { NameUtilisateur = "Pierre" });
+
+        }
+        #endregion
+    }
+
+
+    class Utilisateur
+    {
+        public String NameUtilisateur { get; set; }
+    }
+}
