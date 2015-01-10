dec831575c9bc0e37f3591280b749b9c1b3f0f22
p.taquet@groupe-esigelec.fr <p.taquet@groupe-esigelec.fr@f6d911b7-a069-8245-1612-9514350c5421>
2015-01-10 14:13:08 +0000

    commit

--- trunk/App_pressing_Loreau/View/UCAdministrateur/ParametreAffichage.xaml.cs
+++ trunk/App_pressing_Loreau/View/UCAdministrateur/ParametreAffichage.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace App_pressing_Loreau
+namespace App_pressing_Loreau.View
 {
     /// <summary>
     /// Logique d'interaction pour ParametreAffichage.xaml
