316e4395fe6357bc11905d7d1c9358e603575d6e
alexis.nagalo23@gmail.com <alexis.nagalo23@gmail.com@f6d911b7-a069-8245-1612-9514350c5421>
2015-01-15 10:28:45 +0000

    Recherche client, construction de l'objet AutoComplete et Fields. Permet le traitement des champs de text à chaque entrée

--- App_pressing_Loreau/View/UCReception/IdentificationClient.xaml.cs
+++ App_pressing_Loreau/View/UCReception/IdentificationClient.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using App_pressing_Loreau.ViewModel;
+using System.Windows;
 using System.Windows.Controls;
 
 
@@ -32,5 +33,58 @@ namespace App_pressing_Loreau.View
             dp.Children.Add(new NouvelleCommande());
         }
 
+        private void txb_identificationClient_nom_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            //MessageBox.Show("salut");
+            Fields fields = AutoComplete.getFields();
+            //MessageBox.Show(sender.ToString());
+
+            fields.nom = AutoComplete.parseFromClassToMessage(sender.ToString());
+            //message();
+
+        }
+
+        private void txb_identificationClient_prenom_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            Fields fields = AutoComplete.getFields();
+            fields.prenom = AutoComplete.parseFromClassToMessage(sender.ToString());
+            //message();
+        }
+
+        private void txb_identificationClient_portable_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            Fields fields = AutoComplete.getFields();
+            fields.portable = AutoComplete.parseFromClassToMessage(sender.ToString());
+            //MessageBox.Show(fields.nom);
+            //message();
+
+        }
+
+        private void txb_identificationClient_adresse_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            Fields fields = AutoComplete.getFields();
+            fields.adresse = AutoComplete.parseFromClassToMessage(sender.ToString());
+            //tactacttac
+            //message();
+        }
+
+        private void txb_identificationClient_date_naissance_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            Fields fields = AutoComplete.getFields();
+            fields.dateDeNaissance = AutoComplete.parseFromClassToMessage(sender.ToString());
+            message();
+        }
+
+        private void message(){
+            Fields fields = AutoComplete.getFields();
+            MessageBox.Show("Champs de recherche désirés :\nnom : " + fields.nom +
+                "\nprenom : " + fields.prenom + 
+                "\nportable : " + fields.portable +
+                "\nadresse : " + fields.adresse +
+                "\ndateDeNaissance : " + fields.dateDeNaissance);
+        }
+
+
+        
     }
 }
