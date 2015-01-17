52803270a8c12008426f18041b14e2cc380fcd13
esigelec.ping.1@gmail.com <esigelec.ping.1@gmail.com@f6d911b7-a069-8245-1612-9514350c5421>
2015-01-17 11:56:34 +0000

    Ajout Methode EmployeDAO.cs

--- trunk/App_pressing_Loreau/Model/DTO/Employe.cs
+++ trunk/App_pressing_Loreau/Model/DTO/Employe.cs
@@ -11,18 +11,21 @@ namespace App_pressing_Loreau.Model.DTO
         #region attributs
         public int id { get; set; }
         public string nom { get; set; }
+        public string prenom { get; set; }
         #endregion
 
         #region classes
         public Employe() { }
-        public Employe(string nom)
+        public Employe(string nom, string prenom)
         {
             this.nom = nom;
+            this.prenom = prenom;
         }
-        public Employe(int id, string nom)
+        public Employe(int id, string nom, string prenom)
         {
             this.id = id;
             this.nom = nom;
+            this.prenom = prenom;
         }
         #endregion
     }
