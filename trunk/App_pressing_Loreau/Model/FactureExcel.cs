dc434299da1749c162600c60c5c0eb68a07d70ce
esigelec.ping.1@gmail.com <esigelec.ping.1@gmail.com@f6d911b7-a069-8245-1612-9514350c5421>
2015-01-27 11:48:35 +0000

    Facture Excel (avant test)

---
+++ trunk/App_pressing_Loreau/Model/FactureExcel.cs
@@ -0,0 +1,101 @@
+﻿using App_pressing_Loreau.Model.DTO;
+using Microsoft.Office.Interop.Excel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App_pressing_Loreau.Model
+{
+    class FactureExcel
+    {
+        #region parametres LogExcel
+
+        public Commande commande;
+        
+        public static String pattern_path = "I:\\PING\\App_pressing_loreau\\App_pressing_Loreau\\Resources\\PatternExcel\\FactureExcel";
+        private Microsoft.Office.Interop.Excel.Application oXL;
+        private Microsoft.Office.Interop.Excel.Workbook mWorkBook;
+        private Microsoft.Office.Interop.Excel.Worksheet mWorkSheets;
+        private static object misValue = System.Reflection.Missing.Value;
+
+        #endregion
+
+        #region methodes
+
+        public FactureExcel(Commande commandeWithClient)
+        {
+            commande = commandeWithClient;
+        }
+
+        public void createFacture()
+        {
+            oXL = new Application();
+            mWorkBook = oXL.Workbooks.Open(pattern_path + ".xlsx", 0, true, 5, "", "", true, Microsoft.Office.Interop.Excel.XlPlatform.xlWindows, "\t", false, false, 0, true, 1, 0);
+            mWorkSheets = (Worksheet)mWorkBook.Worksheets.get_Item(1);
+
+            //Ajout de la référence
+            mWorkSheets.Cells[8, 1] = DateTime.Now.ToString("dd/MM/yyyy");
+            mWorkSheets.Cells[9, 2] = commande.id;
+
+            //Ajout des informations client
+            mWorkSheets.Cells[12, 1] = String.Format("{0} {1}", commande.client.nom.ToUpper(), commande.client.prenom);
+            mWorkSheets.Cells[13, 1] = String.Format("{0} {1}", commande.client.adresse.numero, commande.client.adresse.rue);
+            mWorkSheets.Cells[14, 1] = String.Format("{0}", commande.client.adresse.complement);
+            mWorkSheets.Cells[15, 1] = String.Format("{0} {1}", commande.client.adresse.codePostal, commande.client.adresse.ville.ToUpper());
+
+            //ajout des articles
+            int index = 19;
+            float total = 0;
+            float tva = 0;
+            foreach (Article art in commande.listArticles)
+            {
+                mWorkSheets.Cells[index, 2] = art.type.nom;
+                mWorkSheets.Cells[index, 9] = art.TTC*(1-art.TVA/100);
+                total = total + art.TTC * (1 - art.TVA / 100);
+                tva = tva + art.TTC * (art.TVA / 100);
+
+                index++;
+            }
+
+            index += 2;
+            mWorkSheets.Cells[index, 7] = "TVA :";
+            mWorkSheets.Cells[index, 9] = tva;
+            mWorkSheets.Cells[index+1, 7] = "Remise :";
+            mWorkSheets.Cells[index, 9] = commande.remise;
+            mWorkSheets.Cells[index + 2, 7] = "Total :";
+            mWorkSheets.Cells[index, 9] = total + tva - commande.remise;
+
+            //close files
+            oXL.Quit();
+            releaseObject(mWorkSheets);
+            releaseObject(mWorkBook);
+            releaseObject(oXL);
+        }
+
+        public void printFacture()
+        {
+            mWorkSheets.PrintOut(1, 2, 1, false, misValue, true, false, misValue);
+        }
+
+        private void releaseObject(object obj)
+        {
+            try
+            {
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(obj);
+                obj = null;
+            }
+            catch (Exception ex)
+            {
+                obj = null;
+            }
+            finally
+            {
+                GC.Collect();
+            }
+        }
+
+        #endregion
+    }
+}
