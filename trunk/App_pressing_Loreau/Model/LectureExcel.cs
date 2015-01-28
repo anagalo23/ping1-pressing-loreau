7889aa5212ce943246b5907431756c98f18932f8
esigelec.ping.1@gmail.com <esigelec.ping.1@gmail.com@f6d911b7-a069-8245-1612-9514350c5421>
2015-01-28 14:54:52 +0000

    Debut de statistiques pour le lectureExcel

---
+++ trunk/App_pressing_Loreau/Model/LectureExcel.cs
@@ -0,0 +1,56 @@
+﻿using App_pressing_Loreau.Data.DAO;
+using App_pressing_Loreau.Model.DTO;
+using Microsoft.Office.Interop.Excel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App_pressing_Loreau.Model
+{
+    class LectureExcel
+    {
+
+        #region parametres
+        //autres paramètres
+        int type; //0 : LectureX // 1 : LectureZ
+
+        //informations départements
+        List<Departement> listUsedDepartements;
+        List<float> caTTCDep;
+
+        //informations moyen de paiement
+        List<TypePayement> listUsedTypePaiement;
+        List<float> caTTCpaiement;
+
+        //informations type article
+        List<TypeArticle> listUsedTypeArticle;
+        List<int> reçutArticle;
+        List<int> renduArticle;
+
+        //autres paramètres statistiques
+        int nbNewCommande;
+        int nbNewClient;
+
+        //Excel Interop
+        public static String pattern_path = "I:\\PING\\App_pressing_loreau\\App_pressing_Loreau\\Resources\\PatternExcel\\LecturePattern";
+        private Microsoft.Office.Interop.Excel.Application oXL;
+        private Microsoft.Office.Interop.Excel.Workbook mWorkBook;
+        private Microsoft.Office.Interop.Excel.Worksheet mWorkSheets;
+        private static object misValue = System.Reflection.Missing.Value;
+
+        #endregion
+
+        #region methodes
+
+        public LectureExcel(int type)
+        {
+            this.type = type;
+            listUsedTypePaiement = PayementDAO.;
+        }
+
+        
+        #endregion
+    }
+}
