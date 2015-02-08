f72777caa110a0e49a5f0af304bcaef214990396
esigelec.ping.1@gmail.com <esigelec.ping.1@gmail.com@f6d911b7-a069-8245-1612-9514350c5421>
2015-02-08 21:33:32 +0000

    modification du chemin d'accès des imprimentes

--- App_pressing_Loreau/Model/FactureExcel.cs
+++ App_pressing_Loreau/Model/FactureExcel.cs
@@ -17,7 +17,8 @@ namespace App_pressing_Loreau.Model
 
         public static String printName = "Canon MG2400 series";
 
-        public static String pattern_path = AppDomain.CurrentDomain.BaseDirectory.Substring(0, AppDomain.CurrentDomain.BaseDirectory.Length - 10) + "Resources\\PatternFile\\FacturePattern.xlsx";
+        //public static String pattern_path = AppDomain.CurrentDomain.BaseDirectory.Substring(0, AppDomain.CurrentDomain.BaseDirectory.Length - 10) + "Resources\\PatternFile\\FacturePattern.xlsx";
+        public static String pattern_path = "C:\\Programmes\\Application_Pressing\\Resources\\PatternFile\\FacturePattern.xlsx";
         private Microsoft.Office.Interop.Excel.Application oXL;
         private Microsoft.Office.Interop.Excel.Workbook mWorkBook;
         private Microsoft.Office.Interop.Excel.Worksheet mWorkSheets;
