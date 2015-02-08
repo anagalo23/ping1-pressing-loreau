9c1dea2f0e34665142893277a82404766448a143
esigelec.ping.1@gmail.com <esigelec.ping.1@gmail.com@f6d911b7-a069-8245-1612-9514350c5421>
2015-02-08 14:34:27 +0000

    proterties OK

---
+++ App_pressing_Loreau/Model/CashProperties.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.Office.Interop.Excel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App_pressing_Loreau.Model
+{
+    class CashProperties
+    {
+        #region parametres Excel
+
+        public static String pattern_path = AppDomain.CurrentDomain.BaseDirectory.Substring(0, AppDomain.CurrentDomain.BaseDirectory.Length - 10) + "Resources\\properties.xlsx";
+        private static Microsoft.Office.Interop.Excel.Application oXL;
+        private static Microsoft.Office.Interop.Excel.Workbook mWorkBook;
+        private static Microsoft.Office.Interop.Excel.Worksheet mWorkSheets;
+        private static object misValue = System.Reflection.Missing.Value;
+
+        #endregion
+
+        #region parametres preperties
+        public static float fondCaisse { get; set; }
+        
+        #endregion
+
+        #region methodes
+
+        public static void openProperties()
+        {
+            //Ouvre le fichier excel
+            oXL = new Microsoft.Office.Interop.Excel.Application();
+            mWorkBook = oXL.Workbooks.Open(pattern_path, 0, true, 5, "", "", true, Microsoft.Office.Interop.Excel.XlPlatform.xlWindows, "\t", false, false, 0, true, 1, 0);
+            mWorkSheets = (Worksheet)mWorkBook.Worksheets.get_Item(1);
+        }
+
+        public static void selectProperties()
+        {
+            openProperties();
+
+            //récupération du fond de caisse
+            if (mWorkSheets.Cells[2, 3].Value != null)
+                fondCaisse = mWorkSheets.Cells[2, 3].Value;
+
+            closeProperties();
+        }
+        
+        public static void insertProperties(float fdc)
+        {
+            openProperties();
+            //insertion du fond de caisse
+            mWorkSheets.Cells[2, 3] = fdc;
+
+            //save
+            mWorkBook.SaveAs(pattern_path, Microsoft.Office.Interop.Excel.XlFileFormat.xlWorkbookDefault, Type.Missing, Type.Missing,
+            false, false, Microsoft.Office.Interop.Excel.XlSaveAsAccessMode.xlNoChange,
+            Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing);
+
+            //close
+            closeProperties();
+            selectProperties();
+        }
+
+        public static void closeProperties()
+        {
+                //close files
+                mWorkBook.Close(false, misValue, misValue);
+                oXL.Quit();
+
+                //release file
+                releaseObject(mWorkSheets);
+                releaseObject(mWorkBook);
+                releaseObject(oXL);
+
+        }
+
+        private static void releaseObject(object obj)
+        {
+            try
+            {
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(obj);
+                obj = null;
+            }
+            catch (Exception ex)
+            {
+                obj = null;
+            }
+            finally
+            {
+                GC.Collect();
+            }
+        }
+
+        #endregion
+    }
+}
