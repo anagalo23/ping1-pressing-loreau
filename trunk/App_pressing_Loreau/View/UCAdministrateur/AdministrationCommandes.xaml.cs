8e897a08accefe2da082c34fbd0dedd507eee617
alexis.nagalo23@gmail.com <alexis.nagalo23@gmail.com@f6d911b7-a069-8245-1612-9514350c5421>
2015-02-09 18:36:20 +0000

    Commit: -Ajout AdministrationCommandesVM.cs -amelioration AdministrationCommandes -insertion login et mot de passe (login: David et mp : Lefevre)

--- trunk/App_pressing_Loreau/View/UCAdministrateur/AdministrationCommandes.xaml.cs
+++ trunk/App_pressing_Loreau/View/UCAdministrateur/AdministrationCommandes.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using App_pressing_Loreau.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,7 @@ namespace App_pressing_Loreau.View
         public AdministrationCommandes()
         {
             InitializeComponent();
+            DataContext = new AdministrationCommandesVM();
         }
 
         private void btn_admin_commandes_modifier_Click(object sender, RoutedEventArgs e)
---
+++ trunk/App_pressing_Loreau/ViewModel/VMAdministrateur/AdministrationCommandesVM.cs
@@ -0,0 +1,100 @@
+﻿using App_pressing_Loreau.Data.DAO;
+using App_pressing_Loreau.Model.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App_pressing_Loreau.ViewModel
+{
+    class AdministrationCommandesVM : ObservableObject
+    {
+        #region Attributes
+        private List<ItemCommand> _listeCommandeEnCours;
+        #endregion
+
+        #region Constructor
+        public AdministrationCommandesVM()
+        {
+            AfficherCommandeEnCours();
+        }
+        #endregion
+
+        #region Properties and commands
+
+        public List<ItemCommand> ListeCommandeEnCours
+        {
+            get { return _listeCommandeEnCours; }
+            set
+            {
+                if (value != _listeCommandeEnCours)
+                {
+                    _listeCommandeEnCours = value;
+                    OnPropertyChanged("ListeCommandeEnCours");
+                }
+            }
+        }
+        #endregion
+
+        #region Methods
+
+        private void AfficherCommandeEnCours()
+        {
+            ListeCommandeEnCours = new List<ItemCommand>();
+            List<Commande> listeCommande = CommandeDAO.selectCommandes(true, true, true);
+
+            foreach (Commande com in listeCommande)
+            {
+                if (com.date_rendu != null)
+                {
+                    String etat = null;
+                    if (com.payee == true)
+                    {
+                        etat = "payée";
+                    }
+                    else etat = "non payée";
+
+                    float prixP = 0;
+                    float prixA = 0;
+
+                    foreach (Payement pay in com.listPayements)
+                    {
+                        prixP = (float)((decimal)prixP + (decimal)pay.montant);
+                    }
+
+                    foreach (Article art in com.listArticles)
+                    {
+                        prixA = (float)((decimal)prixA + (decimal)art.TTC);
+
+                    }
+                    ListeCommandeEnCours.Add(new ItemCommand()
+                    {
+                        Label_AdminCom_Client = com.client.prenom,
+                        Label_AdminCom_ref = "Ref:   " + com.id,
+                        Label_AdminCom_DateEnregistrement = "Date recu:  " + com.date_rendu,
+                        Label_AdminCom_EtatPaiement = "Etat paiement: " + etat,
+                        Label_AdminCom_PrixRestant = "Prix à payer: " + (float)((decimal)prixA - (decimal)prixP)
+                    });
+                }
+            }
+        }
+
+        #endregion
+
+        #region Class
+
+        public class ItemCommand
+        {
+            public String Label_AdminCom_ref { get; set; }
+            public String Label_AdminCom_EtatPaiement { get; set; }
+            public String Label_AdminCom_DateEnregistrement { get; set; }
+            public String Label_AdminCom_PrixRestant { get; set; }
+            public String Label_AdminCom_Client { get; set; }
+
+
+        }
+        #endregion
+
+    }
+}
