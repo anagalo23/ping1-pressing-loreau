7499eb6f5e84bb41a54d269b00acf72013b2c5f5
esigelec.ping.1@gmail.com <esigelec.ping.1@gmail.com@f6d911b7-a069-8245-1612-9514350c5421>
2015-02-11 20:22:48 +0000

    Gestion de la fermeture des ficiers Excel en cas d'erreurs

--- trunk/App_pressing_Loreau/Model/FactureExcel.cs
+++ trunk/App_pressing_Loreau/Model/FactureExcel.cs
@@ -37,47 +37,62 @@ namespace App_pressing_Loreau.Model
         {
             try
             {
-                            //Ouvre le fichier excel
-            oXL = new Microsoft.Office.Interop.Excel.Application();
-            mWorkBook = oXL.Workbooks.Open(pattern_path, 0, true, 5, "", "", true, Microsoft.Office.Interop.Excel.XlPlatform.xlWindows, "\t", false, false, 0, true, 1, 0);
-            mWorkSheets = (Worksheet)mWorkBook.Worksheets.get_Item(1);
+                //Ouvre le fichier excel
+                oXL = new Microsoft.Office.Interop.Excel.Application();
+                mWorkBook = oXL.Workbooks.Open(pattern_path, 0, true, 5, "", "", true, Microsoft.Office.Interop.Excel.XlPlatform.xlWindows, "\t", false, false, 0, true, 1, 0);
+                mWorkSheets = (Worksheet)mWorkBook.Worksheets.get_Item(1);
 
-            //Ajout de la référence
-            mWorkSheets.Cells[8, 1] = DateTime.Now.ToString("dd/MM/yyyy");
-            mWorkSheets.Cells[9, 2] = commande.id;
+                //Ajout de la référence
+                mWorkSheets.Cells[8, 1] = DateTime.Now.ToString("dd/MM/yyyy");
+                mWorkSheets.Cells[9, 2] = commande.id;
 
-            //Ajout des informations client
-            mWorkSheets.Cells[12, 1] = String.Format("{0} {1}", commande.client.nom.ToUpper(), commande.client.prenom);
-            mWorkSheets.Cells[13, 1] = String.Format("{0} {1}", commande.client.adresse.numero, commande.client.adresse.rue);
-            mWorkSheets.Cells[14, 1] = String.Format("{0}", commande.client.adresse.complement);
-            mWorkSheets.Cells[15, 1] = String.Format("{0} {1}", commande.client.adresse.codePostal, commande.client.adresse.ville.ToUpper());
+                //Ajout des informations client
+                mWorkSheets.Cells[12, 1] = String.Format("{0} {1}", commande.client.nom.ToUpper(), commande.client.prenom);
+                mWorkSheets.Cells[13, 1] = String.Format("{0} {1}", commande.client.adresse.numero, commande.client.adresse.rue);
+                mWorkSheets.Cells[14, 1] = String.Format("{0}", commande.client.adresse.complement);
+                mWorkSheets.Cells[15, 1] = String.Format("{0} {1}", commande.client.adresse.codePostal, commande.client.adresse.ville.ToUpper());
 
-            //ajout des articles
-            int index = 19;
-            decimal total = 0;
-            decimal tva = 0;
-            foreach (Article art in commande.listArticles)
-            {
-                mWorkSheets.Cells[index, 2] = art.type.nom;
-                mWorkSheets.Cells[index, 9] = (decimal)art.TTC * (1 - (decimal)art.TVA / 100);
-                total = (decimal)total + (decimal)art.TTC * (1 - (decimal)art.TVA / 100);
-                tva = tva + (decimal)art.TTC * ((decimal)art.TVA / 100);
+                //ajout des articles
+                int index = 19;
+                decimal total = 0;
+                decimal tva = 0;
+                foreach (Article art in commande.listArticles)
+                {
+                    mWorkSheets.Cells[index, 2] = art.type.nom;
+                    mWorkSheets.Cells[index, 9] = (decimal)art.TTC * (1 - (decimal)art.TVA / 100);
+                    total = (decimal)total + (decimal)art.TTC * (1 - (decimal)art.TVA / 100);
+                    tva = tva + (decimal)art.TTC * ((decimal)art.TVA / 100);
 
-                index++;
-            }
+                    index++;
+                }
 
-            //ajout du total
-            index += 2;
-            mWorkSheets.Cells[index, 7] = "TVA :";
-            mWorkSheets.Cells[index, 9] = tva;
-            mWorkSheets.Cells[index + 1, 7] = "Remise :";
-            mWorkSheets.Cells[index + 1, 9] = commande.remise;
-            mWorkSheets.Cells[index + 2, 7] = "Total :";
-            mWorkSheets.Cells[index + 2, 9] = total + tva - (decimal)commande.remise;
+                //ajout du total
+                index += 2;
+                mWorkSheets.Cells[index, 7] = "TVA :";
+                mWorkSheets.Cells[index, 9] = tva;
+                mWorkSheets.Cells[index + 1, 7] = "Remise :";
+                mWorkSheets.Cells[index + 1, 9] = commande.remise;
+                mWorkSheets.Cells[index + 2, 7] = "Total :";
+                mWorkSheets.Cells[index + 2, 9] = total + tva - (decimal)commande.remise;
             }
             catch (Exception e)
             {
                 MessageBox.Show("Impossible d'ouvrir le fichier FacturePattern.xlsx");
+                try
+                {
+                    //close files
+                    mWorkBook.Close(false, misValue, misValue);
+                    oXL.Quit();
+
+                    //release file
+                    releaseObject(mWorkSheets);
+                    releaseObject(mWorkBook);
+                    releaseObject(oXL);
+                }
+                catch
+                {
+
+                }
             }
 
 
@@ -85,8 +100,8 @@ namespace App_pressing_Loreau.Model
 
         public void printFacture()
         {
-            /*try
-            {*/
+            try
+            {
                 createFacture();
                 /*
                  *@param From : The number of the page at which to start printing. If this argument is omitted, printing starts at the beginning.
@@ -108,9 +123,25 @@ namespace App_pressing_Loreau.Model
                 releaseObject(mWorkSheets);
                 releaseObject(mWorkBook);
                 releaseObject(oXL);
-           /* }
+            }
             catch (Exception e)
-            { }*/
+            {
+                try
+                {
+                    //close files
+                    mWorkBook.Close(false, misValue, misValue);
+                    oXL.Quit();
+
+                    //release file
+                    releaseObject(mWorkSheets);
+                    releaseObject(mWorkBook);
+                    releaseObject(oXL);
+                }
+                catch
+                {
+
+                }
+            }
 
         }
 
