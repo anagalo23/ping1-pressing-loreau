76ae481f898cc7df7ead6df293ede59448f92262
esigelec.ping.1@gmail.com <esigelec.ping.1@gmail.com@f6d911b7-a069-8245-1612-9514350c5421>
2015-02-11 23:49:30 +0000

    modification contenue ticket vetement

--- App_pressing_Loreau/Model/TicketVetement.cs
+++ App_pressing_Loreau/Model/TicketVetement.cs
@@ -73,7 +73,8 @@ namespace App_pressing_Loreau.Model
             System.IO.File.Copy(pattern_path + ".txt", copy_path + ".txt");
 
             //Ajout du contenue du ticket
-            File.AppendAllText(copy_path + ".txt", "Commande " + cmd_id + Environment.NewLine);
+            File.AppendAllText(copy_path + ".txt", "Cmd " + cmd_id + "     Conv "+ art.convoyeur.id + Environment.NewLine);
+            File.AppendAllText(copy_path + ".txt", "Date Depot : " + cmd.date.ToString("dd/MM/yyyy") + Environment.NewLine);
             File.AppendAllText(copy_path + ".txt", "_________________________" + Environment.NewLine);
 
             PrintOff();
@@ -90,7 +91,7 @@ namespace App_pressing_Loreau.Model
                 //Create a StreamReader object
                 reader = new StreamReader(filename);
                 //Create a Verdana font with size 10
-                verdana10Font = new Font("Verdana", 25);
+                verdana10Font = new Font("Verdana", 20);
                 //Create a PrintDocument object
                 PrintDocument pd = new PrintDocument();
                 //gestion des marges
