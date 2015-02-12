dac886db3d142586362dced66bd00ef1b3a771a5
p.taquet@groupe-esigelec.fr <p.taquet@groupe-esigelec.fr@f6d911b7-a069-8245-1612-9514350c5421>
2015-02-12 14:54:23 +0000

    Client pro : rendre des articles

--- trunk/App_pressing_Loreau/ViewModel/VMCLientPro/ClientPROVM.cs
+++ trunk/App_pressing_Loreau/ViewModel/VMCLientPro/ClientPROVM.cs
@@ -102,49 +102,98 @@ namespace App_pressing_Loreau.ViewModel
                 OnPropertyChanged("Label_ClientProUC_TotalAPayer");
             }
         }
-        public ICommand TestImprimante
+        //public ICommand TestImprimante
+        //{
+        //    get { return new RelayCommand(p => testPrint()); }
+        //}
+
+
+        public ICommand Regler_commande_pro
         {
-            get { return new RelayCommand(p => testPrint()); }
+            get { return new RelayCommand(p => ReglerCommande()); }
+        }
+
+        private void ReglerCommande()
+        {
+            //Les articles et la commane passe à payés
+            //TODO
+            MessageBox.Show("Réglage de la commande");
+        }
+
+
+
+
+        public ICommand Rendre_commande_pro
+        {
+            get { return new RelayCommand(p => RendreCommande()); }
+        }
+
+        private void RendreCommande()
+        {
+            //Les articles passent en rendus
+            //TODO
+            //MessageBox.Show("Réglage de la commande");
+            if (ClasseGlobale._renduCommandeClientPro != null)
+            {
+                Commande cmd = ClasseGlobale._renduCommandeClientPro;
+                cmd = CommandeDAO.selectCommandeById(cmd.id, true, true, true);
+                //Parcours de la liste d'article et update de la bdd
+                foreach (Article art in cmd.listArticles)
+                {
+                    //On update l'emplacment convoyeur
+                    art.convoyeur.encombrement -= art.type.encombrement;
+                    PlaceConvoyeurDAO.updatePlaceConvoyeur(art.convoyeur);
+
+                    //Puis l'article
+                    art.date_rendu = DateTime.Now;
+                    art.ifRendu = true;
+                    art.convoyeur = null;
+                    ArticleDAO.updateArticle(art);
+                }
+                cmd.date_rendu = DateTime.Now;
+                CommandeDAO.updateCommande(cmd);
+            }
+
         }
         #endregion
 
         #region Methods
 
 
-        private void testPrint()
-        {
+        //private void testPrint()
+        //{
 
-            Client c = new Client(1, "TRAORE", "Mami", "", "", null, DateTime.Now, "", DateTime.Now, 10, false, false, 0);
-            Commande comTest = new Commande(2, DateTime.Now, false, 0, c);
-            Departement dep = new Departement(1, "Classique");
-            TypeArticle t = new TypeArticle(1, "jupe", 1, 20, 5, dep);
-            TypeArticle t2 = new TypeArticle(2, "robe", 1, 20, 50, dep);
-            TypeArticle t3 = new TypeArticle(3, "habit", 1, 20, 25, dep);
+        //    Client c = new Client(1, "TRAORE", "Mami", "", "", null, DateTime.Now, "", DateTime.Now, 10, false, false, 0);
+        //    Commande comTest = new Commande(2, DateTime.Now, false, 0, c);
+        //    Departement dep = new Departement(1, "Classique");
+        //    TypeArticle t = new TypeArticle(1, "jupe", 1, 20, 5, dep);
+        //    TypeArticle t2 = new TypeArticle(2, "robe", 1, 20, 50, dep);
+        //    TypeArticle t3 = new TypeArticle(3, "habit", 1, 20, 25, dep);
 
-            Article a1 = new Article(1, "", "", false, 20, 5, t, null, 2);
-            Article a2 = new Article(2, "", "", false, 20, 50, t2, null, 2);
-            Article a3 = new Article(3, "", "", false, 20, 25, t3, null, 2);
-            comTest.addArticle(a1);
-            comTest.addArticle(a2);
-            comTest.addArticle(a3);
+        //    Article a1 = new Article(1, "", "", false, 20, 5, t, null, 2);
+        //    Article a2 = new Article(2, "", "", false, 20, 50, t2, null, 2);
+        //    Article a3 = new Article(3, "", "", false, 20, 25, t3, null, 2);
+        //    comTest.addArticle(a1);
+        //    comTest.addArticle(a2);
+        //    comTest.addArticle(a3);
 
 
-            //RecuPaiement rp = new RecuPaiement(comTest);
-            //rp.printRecu();
+        //    //RecuPaiement rp = new RecuPaiement(comTest);
+        //    //rp.printRecu();
 
-            // MessageBox.Show( + "");
-            TicketVetement tv = new TicketVetement(comTest);
-            tv.printTicketVetement(a1, comTest.id, c);
-            //String s=null;
+        //    // MessageBox.Show( + "");
+        //    TicketVetement tv = new TicketVetement(comTest);
+        //    tv.printTicketVetement(a1, comTest.id, c);
+        //    //String s=null;
 
-            //for (int i = 0; i < PrinterSettings.InstalledPrinters.Count; i++)
-            //{
-            //    s += PrinterSettings.InstalledPrinters[i] + "\n";
+        //    //for (int i = 0; i < PrinterSettings.InstalledPrinters.Count; i++)
+        //    //{
+        //    //    s += PrinterSettings.InstalledPrinters[i] + "\n";
 
-            //}
+        //    //}
 
-            //MessageBox.Show(s);
-        }
+        //    //MessageBox.Show(s);
+        //}
 
         private void ExecuteCommandeClientPro(UnClientPROVM obj)
         {
@@ -152,7 +201,8 @@ namespace App_pressing_Loreau.ViewModel
             {
                 ClasseGlobale._renduCommandeClientPro = obj.commande;
                 ClasseGlobale.Client = obj.commande.client;
-                Label_CommandeSelectionner = "Client PRO: " + ClasseGlobale.Client.nom + ",     N° Commande : " + ClasseGlobale._renduCommandeClientPro.id;
+                Label_CommandeSelectionner = "Client PRO: " + ClasseGlobale.Client.nom + ",     N° Commande : " + ClasseGlobale._renduCommandeClientPro.id+
+                    "     TOTAL : " + obj.Label_Detail_prixAPayer;
              
                 Label_ClientProUC_TotalAPayer = obj.Label_Detail_prixAPayer;
             }
